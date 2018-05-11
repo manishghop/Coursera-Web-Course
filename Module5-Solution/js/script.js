b748aa6c9ba9c03e29b788ad16093847ecee8afc
manishghop <manish_coolest44@yahoo.co.in>
2018-05-11 20:02:12 +0530

    Update script.js

--- Module5-Solution/js/script.js
+++ Module5-Solution/js/script.js
@@ -82,8 +82,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // On first load, show home view
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
-  allCategoriesUrl,
-  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
+  allCategoriesUrl,buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -104,6 +103,7 @@ function buildAndShowHomeHTML (categories) {
       var chosenCategoryShortName = chooseRandomCategory(categories);
       console.log(chosenCategoryShortName);
 
+
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
@@ -115,15 +115,16 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      var x="'"+chooseRandomCategory.short_name+"'";
-       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"randomCategoryShortName",x);
+
+       var x = "'" + chosenCategoryShortName.short_name + "'";
+       var homeHtmlToInsertIntoMainPage = insertProperty (homeHtml,"randomCategoryShortName",x);
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      // ....
       insertHtml("#main-content",homeHtmlToInsertIntoMainPage);
+
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
